1f4f771e655e756e68081db2cbd6800b0e0be678
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-04-16 21:06:00 -0500

    [client] angular material and some base components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { SharedModule } from './shared/shared.module';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ContentLayoutComponent, NavComponent, AuthLayoutComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
